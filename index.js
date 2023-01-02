47f5739f7f12840cf7825a2b2cabf0ca32e8f7ca
SantiFP <santiagoferking@gmail.com>
2023-01-01 23:55:18 -0300

    Avance funciones

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const user = document.querySelector('#usuario');
 const pass = document.querySelector('#contraseña');
-const inp = document.getElementsByClassName('input');
+const inp = document.querySelectorAll('.input');
 const login = document.querySelector('#login');
-const exp =  /^[a-zA-Z0-9\_\-]{4,16}$/
+const exp =  /^[a-zA-Z0-9\_\-]{3,16}$/
 
+// inp[0].addEventListener('keyup', (e)=>{
+//     e.preventDefault();
+//     if(exp.test(user.value)){
+//         console.log('Guardo usuario')}
+//     else{
+//         console.log('Usuario incorrecto')
+//     }
+// })
 
-inp.addEventListener('keyup', (e)=>{
-    let texto = e.target.value;
-    console.log(texto)
-    // e.preventDefault();
-    // if(exp.test(user.value)){
-    //     console.log('Guardo usuario')
-    // }else{
-    //     console.log('Usuario incorrecto')
-    // }
-})
-
-
-// login.addEventListener("click", (e)=>{
+// inp[1].addEventListener('keyup', (e)=>{
 //     e.preventDefault();
 //     if(pass.value.length===4 && !isNaN(pass.value)){
 //         console.log('Guardo Contraseña')
@@ -25,5 +21,29 @@ inp.addEventListener('keyup', (e)=>{
 //         console.log('Error contraseña')
 //     }
 // })
+
+login.addEventListener('click', (e)=>{
+    e.preventDefault();
+    if(exp.test(user.value)){
+        console.log('Guardo usuario')}
+    else{
+        console.log('Usuario incorrecto')
+    }
+})
+
+login.addEventListener("click", (e)=>{
+    e.preventDefault();
+    if(pass.value.length===4 && !isNaN(pass.value)){
+        console.log('Guardo Contraseña')
+    }else{
+        console.log('Error contraseña')
+    }
+})
+
+
+// hacer la busqueda de num y letra con dos expresiones regulares
+// disabled habi/deshabi
+// que evento usar para las validaciones
+// color login
 
 
