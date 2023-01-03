4f1da5bad3ea84584dc4b42b4297303d0a5a4859
SantiFP90 <santiagoferking@gmail.com>
2023-01-03 11:59:42 -0300

    Finalizado

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const user = document.querySelector('#usuario');
 const pass = document.querySelector('#contraseña');
 const inp = document.querySelectorAll('#formulario input');
 const login = document.querySelector('#login');
+const cerrarModal = document.querySelector('#cerrarModal');
+const mostrarModal = document.querySelector('#mostrarModal');
 const exp = /^[a-zA-Z0-9]{4,16}$/
 const expN = /^[0-9]*(\.?)[ 0-9]+$/
+const expMin = /^\d{4}$/
 
 
 //Cambiar color login
@@ -18,36 +21,43 @@ check.addEventListener('change', (e)=>{
     }
 })
 
+let ok = 0
+
 //Validar Campos
 const validarForm = (e) =>{
     switch (e.target.name) {
         case "usuario":
             if (exp.test(e.target.value)) {
                 document.getElementById("incorrectoUser").innerHTML =``
-                habilitarLogin()
-
+                ok++;
+                login.disabled = false;
+                login.classList.remove('inhabilitado')
             }else{
                 document.getElementById("incorrectoUser").innerHTML =`
                 <p>Usuario incorrecto </p>
                 `
+                login.disabled = true;
+                login.classList.add('inhabilitado')
             }
             break;
 
         case "contraseña":
-            if (expN.test(e.target.value)) {
+            if (expN.test(e.target.value) && expMin.test(e.target.value)) {
                 document.getElementById("incorrectoPass").innerHTML =``
-                habilitarLogin()
+                ok++;
+                login.disabled = false;
+                login.classList.remove('inhabilitado')
 
             }else{
                 document.getElementById("incorrectoPass").innerHTML =`
                 <p>Constraseña incorrecta </p>
                 `
+                login.disabled = true;
+                login.classList.add('inhabilitado')
             }
             break;
     }
 }
-
-inp.addEventListener('keyup', habilitarLogin)
 
 //Valida si escribe
 inp.forEach((input) => {
@@ -55,45 +65,26 @@ inp.forEach((input) => {
     input.addEventListener('blur', validarForm);
 });
 
-
-
-//Activar boton login
-
-let e1= 0;
-let e2=0;
-
-function habilitarLogin() {
-    if(user.value === ''){
-        e1++;
-    }
-    if(pass.value === ''){
-        e2++;
-    }
-    if(e1 === 0 && e2 === 0){
-        login.value.disabled = false;
-        console.log('aca1')
-    }else{
-        login.value.disabled = true;
-        console.log('aca2')
-    }
+// Modal
+function loginOpen(){
+    mostrarModal.classList.add('modalShow')
+}
+function loginClose(){
+    mostrarModal.classList.remove('modalShow')
 }
 
+// login.addEventListener('click', (e)=>{
+//     if(ok != 0){
+//         console.log('envio')
+//         loginOpen();
+//     }
+// })
 
+document.querySelector('form').addEventListener('submit', (e)=>{
+    if(ok != 0){
+        console.log('envio')
+        loginOpen();
+    }
+})
 
-
-
-
-
-
-
-
-
-
-
-
-// hacer la busqueda de num y letra con dos expresiones regulares
-// disabled habi/deshabi
-// que evento usar para las validaciones
-// color login
-
-
+cerrarModal.addEventListener('click',loginClose)
